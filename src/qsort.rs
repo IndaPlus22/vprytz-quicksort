82cf9647995a0bb6036830242bafd07af680823f
Vilhelm Prytz <vilhelm@prytznet.se>
2023-02-25 19:10:53 +0100

    add another test

--- src/qsort.rs
+++ src/qsort.rs
@@ -5,7 +5,7 @@ pub fn quicksort(arr: &mut [i32]) {
 }
 
 fn qsort(arr: &mut [i32], low: isize, high: isize) {
-    if low <= high {
+    if low < high {
         let p = partition(arr, 0, high);
         qsort(arr, low, p - 1);
         qsort(arr, p + 1, high);
@@ -37,5 +37,17 @@ mod tests {
         let mut arr = [5, 4, 3, 2, 1];
         quicksort(&mut arr);
         assert_eq!(arr, [1, 2, 3, 4, 5]);
+    }
+
+    #[test]
+    fn test_with_larger() {
+        let mut arr = [
+            5, 4, 3, 2, 1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        ];
+        quicksort(&mut arr);
+        assert_eq!(
+            arr,
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        );
     }
 }
