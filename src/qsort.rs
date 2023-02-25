471c8c6d87b768f16fea5707d8d45bc88ed6575c
Vilhelm Prytz <vilhelm@prytznet.se>
2023-02-25 19:34:17 +0100

    add insertion sort for smaller arrays

--- src/qsort.rs
+++ src/qsort.rs
@@ -4,11 +4,25 @@ pub fn quicksort(arr: &mut [i32]) {
     qsort(arr, 0, (arr.len() - 1) as isize);
 }
 
+fn insertion_sort(arr: &mut [i32], low: isize, high: isize) {
+    for i in low + 1..=high {
+        let mut j = i;
+        while j > low && arr[j as usize] < arr[(j - 1) as usize] {
+            arr.swap(j as usize, (j - 1) as usize);
+            j -= 1;
+        }
+    }
+}
+
 fn qsort(arr: &mut [i32], low: isize, high: isize) {
     if low < high {
-        let p = partition(arr, low, high);
-        qsort(arr, low, p - 1);
-        qsort(arr, p + 1, high);
+        if high - low < 50 {
+            insertion_sort(arr, low, high);
+        } else {
+            let p = partition(arr, low, high);
+            qsort(arr, low, p - 1);
+            qsort(arr, p + 1, high);
+        }
     }
 }
 
@@ -40,14 +54,25 @@ mod tests {
     }
 
     #[test]
-    fn test_with_larger() {
+    fn test_qsort_with_larger() {
         let mut arr = [
-            5, 4, 3, 2, 1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            -1, -10, -30, -50, 5, 4, 3, 2, 1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20,
         ];
         quicksort(&mut arr);
         assert_eq!(
             arr,
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+            [
+                -50, -30, -10, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+                19, 20
+            ]
         );
+    }
+
+    #[test]
+    fn test_insertion_sort() {
+        let mut arr = [5, 4, 3, 2, 1];
+        insertion_sort(&mut arr, 0, 4);
+        assert_eq!(arr, [1, 2, 3, 4, 5]);
     }
 }
