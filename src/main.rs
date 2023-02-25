8759091b26a5f04665c313de5a27087e8207d524
Vilhelm Prytz <vilhelm@prytznet.se>
2023-02-25 18:51:19 +0100

    basic working qsort

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@
  * https://github.com/IndaPlus22/AssignmentInstructions-BlueNote/blob/main/task-17/kattis_template/src/main.rs
  */
 
+mod qsort;
+
 use std::io;
 use std::io::prelude::*;
+
+use qsort::quicksort;
 
 /// Kattis calls main function to run your solution.
 fn main() {
@@ -17,19 +21,27 @@ fn main() {
 
     // get input lines as iterative
     let mut lines = input.lock().lines().map(|_line| _line.ok().unwrap());
-    // and get one line at a time,
-    let next_line = lines.next().unwrap();
 
-    // or loop all input lines,
-    for _line in input.lock().lines().map(|_line| _line.unwrap()) {
-        // ...
+    // get one line at a time, until EOF
+    while let Some(line) = lines.next() {
+        // input should be like "5 4 3 2 1", this we want to turn into i32 array
+        let mut arr: Vec<i32> = line
+            .split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+
+        // sort the array
+        quicksort(&mut arr);
+
+        // print the sorted array
+        println!(
+            "{}",
+            arr.iter()
+                .map(|i| i.to_string())
+                .collect::<Vec<String>>()
+                .join(" ")
+        );
     }
-
-    // or read single line
-    let mut line = String::new();
-    input.read_line(&mut line).expect("IO Error");
-
-    /* add code here ... */
 
     eprintln!("Kattis skips this comment!");
     //println!("Print to standard output.");
---
+++ src/qsort.rs
@@ -0,0 +1,41 @@
+// quicksort implementation
+
+pub fn quicksort(arr: &mut [i32]) {
+    qsort(arr, 0, (arr.len() - 1) as isize);
+}
+
+fn qsort(arr: &mut [i32], low: isize, high: isize) {
+    if low <= high {
+        let p = partition(arr, 0, high);
+        qsort(arr, low, p - 1);
+        qsort(arr, p + 1, high);
+    }
+}
+
+// Lomuto algorithm:
+fn partition(arr: &mut [i32], low: isize, high: isize) -> isize {
+    let pivot = high;
+    let mut i = low as isize - 1;
+
+    for j in low..=high - 1 {
+        if arr[j as usize] <= arr[pivot as usize] {
+            i += 1;
+            arr.swap(i as usize, j as usize);
+        }
+    }
+    arr.swap((i + 1) as usize, pivot as usize);
+    i + 1
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_qsort() {
+        let mut arr = [5, 4, 3, 2, 1];
+        quicksort(&mut arr);
+        assert_eq!(arr, [1, 2, 3, 4, 5]);
+    }
+}
